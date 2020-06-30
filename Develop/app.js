31d376993f58c138560d869ab7338bd66508c138
Eliza Regas <elizaregas@gmail.com>
2020-06-30 17:04:22 -0400

    refactoring prompt questions into json files

--- Develop/app.js
+++ Develop/app.js
@@ -4,6 +4,10 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const managerQuestions = require("./data/managerQuestions.json");
+const employeeType = require("./data/employeeType.json");
+const engineerQuestions = require("./data/engineerQuestions.json");
+const internQuestions = require("./data/internQuestions.json");
 let employeeInfo = [];
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
@@ -11,32 +15,9 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-function getManagerInfo() {
+const getManagerInfo = () => {
   inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "What is your manager's name?",
-      },
-      {
-        type: "input",
-        name: "id",
-        message: "What is your manager's ID?",
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "What is your manager's email?",
-      },
-      {
-        type: "input",
-        name: "officeNumber",
-        message: "What is your manager's office number?",
-      },
-    ])
+    .prompt(managerQuestions)
     .then((results) => {
       results.employeeType = "Manager";
       continueGatheringEmployeeInfo(results);
@@ -48,42 +29,14 @@ function getManagerInfo() {
 
 getManagerInfo();
 
-function enterEmployeeInfo() {
+const enterEmployeeInfo = () => {
   inquirer
-    .prompt([
-      {
-        type: "list",
-        name: "employeeType",
-        message: "Which type of team member would you like to add?",
-        choices: ["Engineer", "Intern"],
-      },
-    ])
+    .prompt(employeeType)
     .then((results) => {
       switch (results.employeeType) {
         case "Engineer":
           inquirer
-            .prompt([
-              {
-                type: "input",
-                name: "name",
-                message: "What is your engineer's name?",
-              },
-              {
-                type: "input",
-                name: "id",
-                message: "What is your engineer's ID?",
-              },
-              {
-                type: "input",
-                name: "email",
-                message: "What is your engineer's email?",
-              },
-              {
-                type: "input",
-                name: "gitHubUserName",
-                message: "What is your engineer's GitHub username?",
-              },
-            ])
+            .prompt(engineerQuestions)
             .then((res) => {
               res.employeeType = "Engineer";
               continueGatheringEmployeeInfo(res);
@@ -94,28 +47,7 @@ function enterEmployeeInfo() {
           break;
         case "Intern":
           inquirer
-            .prompt([
-              {
-                type: "input",
-                name: "name",
-                message: "What is your intern's name?",
-              },
-              {
-                type: "input",
-                name: "id",
-                message: "What is your intern's ID?",
-              },
-              {
-                type: "input",
-                name: "email",
-                message: "What is your intern's email?",
-              },
-              {
-                type: "input",
-                name: "schoolName",
-                message: "What is your intern's school?",
-              },
-            ])
+            .prompt(internQuestions)
             .then((res) => {
               res.employeeType = "Intern";
               continueGatheringEmployeeInfo(res);
@@ -131,7 +63,7 @@ function enterEmployeeInfo() {
     });
 }
 
-function continueGatheringEmployeeInfo(results) {
+const continueGatheringEmployeeInfo = (results) => {
   inquirer
     .prompt([
       {
@@ -159,7 +91,7 @@ function continueGatheringEmployeeInfo(results) {
         enterEmployeeInfo();
       } else {
         const renderInfo = render(employeeInfo);
-        fs.writeFile('./output/index.html', renderInfo, (err) => {
+        fs.writeFile(outputPath, renderInfo, (err) => {
           if (err) throw err;
           console.log('The file has been saved!');
         });
@@ -169,23 +101,3 @@ function continueGatheringEmployeeInfo(results) {
       console.log(err);
     });
 }
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
