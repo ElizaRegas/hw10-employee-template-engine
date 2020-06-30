e2a5d3e7d298540c34f7ae784a21c6908cecb497
Eliza Regas <elizaregas@gmail.com>
2020-06-30 14:48:52 -0400

    all prompts in place

--- Develop/app.js
+++ Develop/app.js
@@ -4,6 +4,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+let employeeInfo = [];
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -12,7 +13,7 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-
+function enterEmployeeInfo() {
 inquirer
   .prompt([
     {
@@ -35,12 +36,7 @@ inquirer
       type: "input",
       name: "email",
       message: "Please enter the employee's email:",
-    },
-    // {
-    //   type: "confirm",
-    //   name: "addAnother",
-    //   message: "Do you wish to add another employee?"
-    // }
+    }
 
     // * getName()
     // * getId()
@@ -48,7 +44,6 @@ inquirer
     // * getRole() // Returns 'Employee'
   ])
   .then((results) => {
-    console.log(results);
     switch (results.employeeType) {
       case "Manager":
         inquirer
@@ -57,10 +52,11 @@ inquirer
               type: "input",
               name: "officeNumber",
               message: "Enter Manager's office number:",
-            },
+            }
           ])
           .then((res) => {
-            console.log(res);
+            results.officeNumber = res.officeNumber;
+            continueGatheringEmployeeInfo(results);
           })
           .catch((err) => {
             console.log(err);
@@ -73,10 +69,11 @@ inquirer
               type: "input",
               name: "gitHubUserName",
               message: "Enter Engineer's GitHub user name:",
-            },
+            }
           ])
           .then((res) => {
-            console.log(res);
+            results.gitHubUserName = res.gitHubUserName;
+            continueGatheringEmployeeInfo(results);
           })
           .catch((err) => {
             console.log(err);
@@ -89,10 +86,11 @@ inquirer
               type: "input",
               name: "schoolName",
               message: "Enter Intern's school name:",
-            },
+            }
           ])
           .then((res) => {
-            console.log(res);
+            results.schoolName = res.schoolName;
+            continueGatheringEmployeeInfo(results);
           })
           .catch((err) => {
             console.log(err);
@@ -103,6 +101,29 @@ inquirer
   .catch((err) => {
     console.log(err);
   });
+}
+
+enterEmployeeInfo();
+
+function continueGatheringEmployeeInfo(results) {
+  inquirer
+    .prompt([
+      {
+        type: "confirm",
+        name: "addAnother",
+        message: "Do you wish to add another employee?"
+      }
+    ]).then((res2) => {
+      employeeInfo.push(results);
+      console.log(employeeInfo);
+      if (res2.addAnother) {
+        enterEmployeeInfo();
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
